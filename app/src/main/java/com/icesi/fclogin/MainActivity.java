e3a0c0f410730d058e6d6c5034df73b6912a24bb
Erik Dubois <jayc.7447@gmail.com>
2019-06-04 07:05:35 -0500

    Initial commit

---
+++ app/src/main/java/com/icesi/fclogin/MainActivity.java
@@ -0,0 +1,86 @@
+package com.icesi.fclogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et_login_email;
+
+    private EditText et_login_password;
+
+    private LoginButton loginButton;
+
+    private CallbackManager callbackManager;
+
+    private static final String EMAIL = "email";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        et_login_email = findViewById(R.id.et_login_email);
+        et_login_password = findViewById(R.id.et_login_password);
+        loginButton = findViewById(R.id.buttonFacebookLogin);
+
+        loginButton.setReadPermissions(Arrays.asList(EMAIL, "public_profile"));
+
+        callbackManager = CallbackManager.Factory.create();
+
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(getApplicationContext(), "Login successful" + loginResult.getAccessToken(), Toast.LENGTH_LONG).show();
+                        //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+
+
+}
